8968cabe2a9d4a03fb2de06cbd5d0918739a8500
Sapphire0912 <iris228520@gmail.com>
2024-02-22 07:14:05 +0800

    Project

--- QuizSystem/QuizUI.py
+++ QuizSystem/QuizUI.py
@@ -1,1 +1,11 @@
-import pyqt5
+from PyQt5 import QtWidgets, QtCore, QtGui
+import sys
+
+
+class InitialWindow(QtWidgets.QWidget):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("教師資格考試測驗系統")
+        self.setWindowIcon(QtGui.QIcon("./windowsicon.ico"))
+        pass
