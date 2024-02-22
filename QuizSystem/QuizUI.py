06077b6843f3eccfc59352da2682be76df5d2c28
Sapphire0912 <iris228520@gmail.com>
2024-02-22 11:00:11 +0800

    Project

--- QuizSystem/QuizUI.py
+++ QuizSystem/QuizUI.py
@@ -1,5 +1,6 @@
 from PyQt5 import QtWidgets, QtCore, QtGui
 import sys
+import cv2
 
 
 class InitialWindow(QtWidgets.QWidget):
@@ -10,10 +11,18 @@ class InitialWindow(QtWidgets.QWidget):
         self.setWindowIcon(QtGui.QIcon("./windowsicon.ico"))
 
         # 初始螢幕視窗大小(固定)
-        self.setFixedSize(1280, 800)
+        self.setFixedSize(1200, 768)
+
+        # ----- 參數設定 -----
+        self.init_title = QtWidgets.QLabel(self)
+        # 調整視窗大小按鈕變數
+        self.winsize_group = QtWidgets.QButtonGroup(self)
+        self.small = QtWidgets.QRadioButton(self)
+        self.median = QtWidgets.QRadioButton(self)
+        self.large = QtWidgets.QRadioButton(self)
+        # -----
 
         self._windows_setting()
-        self._parameter_setting()
         self.ui()
 
     def _windows_setting(self):
@@ -21,49 +30,62 @@ class InitialWindow(QtWidgets.QWidget):
         screen = QtWidgets.QApplication.desktop()
         screen_width, screen_height = screen.width(), screen.height()
         width, height = self.width(), self.height()
-        self.move((screen_width - width)//2, (screen_height - height) // 2)
+        self.move((screen_width - width) // 2, (screen_height - height) // 2)
 
-    def _parameter_setting(self):
-        self.btn_win_select_group = QtWidgets.QButtonGroup(self)
-        self.label2 = QtWidgets.QLabel(self)
+        # 利用視窗大小, 計算每個元件的相對位置
+        # 1. 初始畫面標題
+        init_title_h = int(height // 4)
+        self.init_title.setGeometry(0, 0, width, init_title_h)
 
+        # - 標題背景
+        # - Style
+        self.init_title.setAlignment(QtCore.Qt.AlignCenter | QtCore.Qt.AlignJustify)
+        self.init_title.setStyleSheet(
+            '''
+            color: black;
+            font-size: 72px;
+            font-weight: bold;
+            border: solid black;
+            border-width: 0px 0px 2px 0px;
+            '''
+        )
+
+        start_x = int(self.width() * 0.05)
+        start_y = int(self.height() * 0.8)
+        group_w = int(self.width() // 2) - start_x
+        group_h = 50
+
+        self.small.setGeometry(start_x, start_y, group_w, group_h)
+        self.median.setGeometry(start_x, start_y + group_h, group_w, group_h)
+        self.large.setGeometry(start_x, start_y + group_h * 2, group_w, group_h)
 
     def ui(self):
-        # 設定初始畫面
-        label = QtWidgets.QLabel(self)
-        label.setText('教檢測驗系統')
+        # 設定初始畫面的標題
+        self.init_title.setText('教檢測驗系統')
 
-        # unit test
-        small = QtWidgets.QRadioButton(self)
-        small.setGeometry(30, 30, 100, 20)
-        small.setText('1280x800')
+        # windows size select
+        self.small.setChecked(True)
+        self.small.setText('1200x768')
+        self.median.setText('1600x900')
+        self.large.setText('1920x1080')
 
-        median = QtWidgets.QRadioButton(self)
-        median.setGeometry(30, 60, 100, 20)
-        median.setText('1600x900')
+        self.winsize_group.addButton(self.small, id=1)
+        self.winsize_group.addButton(self.median, id=2)
+        self.winsize_group.addButton(self.large, id=3)
 
-        large = QtWidgets.QRadioButton(self)
-        large.setGeometry(30, 90, 100, 20)
-        large.setText('1920x1080')
-
-        self.btn_win_select_group.addButton(small, id=1)
-        self.btn_win_select_group.addButton(median, id=2)
-        self.btn_win_select_group.addButton(large, id=3)
-
-        self.btn_win_select_group.buttonClicked.connect(self._win_select_event)
-        self.label2.setGeometry(30, 120, 100, 20)
+        self.winsize_group.buttonClicked.connect(self._win_select_event)
         pass
 
     def _win_select_event(self):
-        self.label2.setText(str(self.btn_win_select_group.checkedId()))
-        sizeDict = {
-            1: [1280, 800],
-            2: [1600, 900],
-            3: [1920, 1080]
-        }
-        self.setFixedSize(sizeDict[self.btn_win_select_group.checkedId()][0], sizeDict[self.btn_win_select_group.checkedId()][1])
+        select_id = self.winsize_group.checkedId()
+        if select_id == 1:
+            self.setFixedSize(1280, 800)
+        elif select_id == 2:
+            self.setFixedSize(1600, 900)
+        elif select_id == 3:
+            self.setFixedSize(1920, 1080)
+
         self._windows_setting()
-        pass
 
 
 if __name__ == '__main__':
