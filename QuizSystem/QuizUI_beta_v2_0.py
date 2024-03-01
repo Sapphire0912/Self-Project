a49aa3c8c51ab55652dd431ae2792f0dfb630204
Sapphire0912 <iris228520@gmail.com>
2024-03-01 08:51:59 +0800

    Project

--- QuizSystem/QuizUI_beta_v2_0.py
+++ QuizSystem/QuizUI_beta_v2_0.py
@@ -310,7 +310,7 @@ class InitialWindow(QtWidgets.QWidget):
         self.description.setReadOnly(True)  # Read Only
         self.description.setPlainText('''介面說明：\n1. 下方選擇觀看合適的視窗大小\n2. 在測驗科目中, 選擇想測驗的科目\n\
 3. 在選擇範圍中, 選擇想測驗的試卷年份\n4. 確定選擇正確後, 點選進入測驗的按鈕\n5. 再次確認考試資訊無誤後, 點選確認進入正式測驗\n\n版本說明：
-beta v1.0：新增教育理念與實務、學習者發展與適性輔導 111、112 年試卷，課程教學與評量 112 年試卷\n''')
+beta v2.0：新增教育理念與實務、學習者發展與適性輔導、課程教學與評量 111、112 年試卷\n''')
         pass
 
         # subject title text
@@ -672,7 +672,6 @@ class QuizWindows(QtWidgets.QWidget):
 
         elif question["isImage"] == "Q":
             A, B, C, D = options["A"]["text"], options["B"]["text"], options["C"]["text"], options["D"]["text"]
-
             # 題目有圖片
             q_img = QPixmap(question["Q"]["img"])
             self.question_image.setPixmap(q_img)
