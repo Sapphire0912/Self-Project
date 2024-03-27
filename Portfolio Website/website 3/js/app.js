0d129d45eed18b0f4565fafc703b2530b517753a
Sapphire0912 <iris228520@gmail.com>
2024-03-27 09:28:14 +0800

    Add Home Page menu animation and contact-link

--- Portfolio Website/website 3/js/app.js
+++ Portfolio Website/website 3/js/app.js
@@ -1,11 +1,23 @@
 let menu = document.querySelector(".menu");
 let side_nav = document.querySelector(".side-nav");
+let side_nav_ul = document.querySelector(".side-nav ul");
 let close_btn = document.querySelector(".close-btn");
 
 menu.addEventListener("click", function () {
   side_nav.style.display = "block";
+  side_nav.style.animation = "animate-open 0.5s forwards";
+  setTimeout(() => {
+    side_nav_ul.style.display = "flex";
+    side_nav_ul.style.flexDirection = "column";
+  }, 100);
 });
 
 close_btn.addEventListener("click", function () {
-  side_nav.style.display = "none";
+  side_nav.style.animation = "animate-close 0.5s forwards";
+  setTimeout(() => {
+    side_nav_ul.style.display = "none";
+  }, 150);
+  setTimeout(() => {
+    side_nav.style.display = "none";
+  }, 350);
 });
