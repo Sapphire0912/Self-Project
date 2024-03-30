44450ba767e0cf1c326ceb5cdbd0f28d2d763cce
Sapphire0912 <iris228520@gmail.com>
2024-03-31 07:55:36 +0800

    Add Menu function
    新增選單的功能: 根據使用者位於視窗中的哪個位置, 而更新選單的狀態

--- Portfolio Website/website 3/js/app.js
+++ Portfolio Website/website 3/js/app.js
@@ -1,3 +1,4 @@
+/* 控制所有 header 裡面導覽列選單事件 */
 let menu = document.querySelector(".menu");
 let side_nav = document.querySelector(".side-nav");
 let side_nav_ul = document.querySelector(".side-nav ul");
@@ -20,4 +21,58 @@ close_btn.addEventListener("click", function () {
   setTimeout(() => {
     side_nav.style.display = "none";
   }, 350);
+});
+
+/* 控制 Resume 選單的事件 */
+/* 
+   判斷使用者的視窗正在哪個頁面, 要更新 nav 的點選狀態 
+   : 更新 currentClicked 數值
+*/
+let sectionNav = document.querySelectorAll(".each-nav");
+let contentSection = document.querySelector(".content-section");
+let currentClicked = "";
+
+function removeBeforeStatus() {
+  /* 消除先前 button 的狀態 */
+  sectionNav.forEach((eachNav) => {
+    if (eachNav.classList[1] == currentClicked) {
+      eachNav.children[0].classList.remove("active");
+    }
+  });
+}
+
+window.addEventListener("scroll", () => {
+  let eachIdElement = contentSection.querySelectorAll("[id]");
+  let windowHeight = window.innerHeight;
+  let currentContentId;
+
+  eachIdElement.forEach((content) => {
+    let position = content.getBoundingClientRect();
+    let centerY = windowHeight / 2;
+    if (centerY >= position.top && centerY <= position.bottom) {
+      currentContentId = content.id;
+    }
+  });
+  currentContentId = "#" + currentContentId;
+  sectionNav.forEach((eachNav) => {
+    let href = eachNav.children[1].getAttribute("href");
+    if (href == currentContentId && eachNav.classList[1] != currentClicked) {
+      removeBeforeStatus();
+      currentClicked = eachNav.classList[1];
+      eachNav.children[0].classList.add("active");
+    }
+  });
+});
+
+sectionNav.forEach((eachNav) => {
+  eachNav.addEventListener("click", (event) => {
+    if (event.target.tagName == "A") {
+      let parent = event.target.parentNode.classList[1];
+      if (currentClicked != parent) {
+        removeBeforeStatus();
+        currentClicked = parent;
+        event.target.parentNode.children[0].classList.add("active");
+      }
+    }
+  });
 });
